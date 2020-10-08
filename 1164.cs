fdb5f6baddbcead0d977a9f9d539e7a70ec1495d
Fanzin1 <59770479+Fanzin1@users.noreply.github.com>
2020-10-08 11:21:35 -0300

    Add files via upload

---
+++ 1164.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1164
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < x; i++)
+            {
+                int l = Convert.ToInt32(Console.ReadLine());
+
+                int soma = 0;
+                int g = 0;
+
+                for (int f = 1; g < l; f++)
+                {
+                    if(l % f == 0 && f != l)
+                    {
+                        soma = soma + f;
+                    }
+                    g = g + 1;
+                }           
+                if(soma == l && soma != 1)
+                {
+                    Console.WriteLine("{0} eh perfeito", l);
+                }
+                else
+                {
+                    Console.WriteLine("{0} nao eh perfeito", l);
+                }
+            }
+        }
+    }
+}
